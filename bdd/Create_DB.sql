36f495e9fa86a2a30cbbfd620404b4e3b437cd96
QuentGrd <quentin.gerard2@gmail.com>
2017-11-22 10:07:32 +0100

    Fixed DB Creation

--- bdd/Create_DB.sql
+++ bdd/Create_DB.sql
@@ -1,40 +1,40 @@
-DELETE FROM User;
-DELETE FROM Account;
-DELETE FROM SharedAccount;
 DELETE FROM Settings;
+DELETE FROM SharedAccount;
+DELETE FROM Account;
+DELETE FROM Users;
 
-DROP TABLE User;
-DROP TABLE Account;
-DROP TABLE SharedAccount;
 DROP TABLE Settings;
+DROP TABLE SharedAccount;
+DROP TABLE Account;
+DROP TABLE Users;
 
-CREATE TABLE User{
-	id_user SERIAL;
-	pseudo VARCHAR2(20);
-	mail_adr VARCHAR2(75);
-	password VARCHAR2(100);
-	CONSTRAINT user_pk PRIMARY KEY (id_user);
-}
+CREATE TABLE Users(
+	id_user SERIAL,
+	pseudo VARCHAR(20),
+	mail_adr VARCHAR(75),
+	password VARCHAR(100),
+	CONSTRAINT user_pk PRIMARY KEY (id_user)
+);
 
-CREATE TABLE Account{
-	id_account SERIAL;
-	site VARCHAR2(100);
-	login VARCHAR2(75);
-	password VARCHAR2(100);
-	id_user SERIAL REFERENCES User(id_user);
-	CONSTRAINT account_pk PRIMARY KEY (id_account);
-}
+CREATE TABLE Account(
+	id_account SERIAL,
+	site VARCHAR(100),
+	login VARCHAR(75),
+	password VARCHAR(100),
+	id_user SERIAL REFERENCES Users (id_user),
+	CONSTRAINT account_pk PRIMARY KEY (id_account)
+);
 
-CREATE TABLE SharedAccount{
-	id_share SERIAL;
-	expire_date DATE;
-	id_account SERIAL REFERENCES Account(id_account);
-	id_user SERIAL REFERENCES User(id_user);
-	CONSTRAINT sharedaccount_pk PRIMARY KEY (id_share);
-}
+CREATE TABLE SharedAccount(
+	id_share SERIAL,
+	expire_date DATE,
+	id_account SERIAL REFERENCES Account (id_account),
+	id_user SERIAL REFERENCES Users (id_user),
+	CONSTRAINT sharedaccount_pk PRIMARY KEY (id_share)
+);
 
-CREATE TABLE Settings{
-	id_user SERIAL REFERENCES User(id_user);
-	language VARCHAR2(20);
+CREATE TABLE Settings(
+	id_user SERIAL REFERENCES Users (id_user),
+	language VARCHAR(20),
 	CONSTRAINT settings_pk PRIMARY KEY (id_user)
-}
+);
