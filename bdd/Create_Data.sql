6827d0d3cb32ffadc2063c1e1f206164d6f2a777
QuentGrd <quentin.gerard2@gmail.com>
2017-12-03 15:15:59 +0100

    Add Sites Insertion & Update Tags List into creation data file

--- bdd/Create_Data.sql
+++ bdd/Create_Data.sql
@@ -11,3 +11,16 @@ INSERT INTO Tags (name_tag) VALUES ('Work');
 INSERT INTO Tags (name_tag) VALUES ('Social Network');
 INSERT INTO Tags (name_tag) VALUES ('Tool');
 INSERT INTO Tags (name_tag) VALUES ('Mail');
+INSERT INTO Tags (name_tag) VALUES ('News');
+INSERT INTO Tags (name_tag) VALUES ('Shop');
+INSERT INTO Tags (name_tag) VALUES ('Music');
+
+#Sites Insertion
+INSERT INTO Sites (domain) VALUES ('facebook.com');
+INSERT INTO Sites (domain) VALUES ('twitter.com');
+INSERT INTO Sites (domain) VALUES ('google.com');
+INSERT INTO Sites (domain) VALUES ('trello.com');
+INSERT INTO Sites (domain) VALUES ('evernote.com');
+INSERT INTO Sites (domain) VALUES ('nytimes.com');
+INSERT INTO Sites (domain) VALUES ('amazon.com');
+INSERT INTO Sites (domain) VALUES ('spotify.com');
