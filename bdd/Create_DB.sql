db2c18a19640b83b31ddde91b3b09f8dce1681ca
QuentGrd <quentin.gerard2@gmail.com>
2017-12-04 11:09:46 +0100

    Update DB Creation Script

--- bdd/Create_DB.sql
+++ bdd/Create_DB.sql
@@ -14,14 +14,14 @@ DROP TABLE Users;
 
 CREATE TABLE Users(
 	id_user SERIAL,
-	name VARCHAR(30),
-	first_name VARCHAR(30),
-	gender BOOLEAN,
-	pseudo VARCHAR(20),
-	mail VARCHAR(75),
-	password VARCHAR(100),
-	language VARCHAR(30),
-	creation_date DATE,
+	name VARCHAR(30) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	gender BOOLEAN NOT NULL,
+	pseudo VARCHAR(20) NOT NULL,
+	mail VARCHAR(75) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	language VARCHAR(30) NOT NULL,
+	creation_date DATE NOT NULL,
 	last_conn_date DATE,
 	average_freq_conn INT,
 	current_fred_conn INT,
@@ -30,7 +30,7 @@ CREATE TABLE Users(
 
 CREATE TABLE Sites(
 	id_site SERIAL,
-	domain VARCHAR(30),
+	domain VARCHAR(30) NOT NULL,
 	login_input VARCHAR(30),
 	password_input VARCHAR(30),
 	nb_user_today INT,
@@ -41,15 +41,15 @@ CREATE TABLE Sites(
 
 CREATE TABLE Tags(
 	id_tag SERIAL,
-	name_tag VARCHAR(30),
+	name_tag VARCHAR(30) NOT NULL,
 	CONSTRAINT tags_pk PRIMARY KEY (id_tag)
 );
 
 CREATE TABLE Account(
 	id_account SERIAL,
 	id_site SERIAL REFERENCES Sites (id_site),
-	login VARCHAR(75),
-	password VARCHAR(100),
+	login VARCHAR(75) NOT NULL,
+	password VARCHAR(100) NOT NULL,
 	id_user SERIAL REFERENCES Users (id_user),
 	id_tag SERIAL REFERENCES Tags (id_tag),
 	CONSTRAINT account_pk PRIMARY KEY (id_account)
@@ -57,7 +57,7 @@ CREATE TABLE Account(
 
 CREATE TABLE SharedAccount(
 	id_sharedAccount SERIAL,
-	expiration_date DATE,
+	expiration_date DATE NOT NULL,
 	id_account SERIAL REFERENCES Account (id_account),
 	id_receiver SERIAL REFERENCES Users (id_user),
 	CONSTRAINT sharedaccount_pk PRIMARY KEY (id_sharedAccount)
