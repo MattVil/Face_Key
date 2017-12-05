41e5925ddc71ab1555c841bbc02f17fb264e744c
MattVil <mattvilain@gmail.com>
2017-12-05 21:41:41 +0100

    nouvelle requete

--- bdd/panel/tmp.inc.php
+++ bdd/panel/tmp.inc.php
@@ -1,46 +1,52 @@
 <?php
 function createGraph($user){
-		$ind = decrypt_ind($ind);
 
-		$path = "./users/$user/freq.csv";
+	$path = "./users/$user/freq.csv";
+	$day = array();
+	$nb_conn = array();
 
-		$day = array();
-		$nb_conn = array();
-
-		//lecture CSV
-		if (($handle = fopen($path, "r")) !== FALSE) {
+	//lecture CSV
+	if (($handle = fopen($path, "r")) !== FALSE) {
 			while (($data = fgetcsv($handle, 10000, ";")) !== FALSE) {
-				array_push($nb_conn, $data[2]);
-				array_push($day, $data[1]);
+					array_push($nb_conn, $data[1]);
+					array_push($day, $data[0]);
+					// echo "nb con : " . $data[0] . " | day : " . $data[1] . "<br/>";
 			}
 			fclose($handle);
-		}
-		fclose($handle);
+	}
+	fclose($handle);
 
-	  $width = 600;
-		$height = 300;
+	$width = 600;
+	$height = 300;
 
-	  $graph = new Graph($width,$height,'auto');
-	  $graph->SetScale("intlin");
+	$graph = new Graph($width,$height,'auto');
+	$graph->SetScale("intlin");
 
-		$graph->SetShadow();
-		$graph->SetMargin(40,30,20,40);
+	$graph->SetShadow();
+	$graph->SetMargin(40,30,20,40);
 
-		$b1plot = new BarPlot($nb_conn);
-		$bplot->SetFillColor('orange');
-		$graph->Add($b1plot);
+	$b1plot = new BarPlot($nb_conn);
+	$b1plot->SetFillColor('orange');
+
+	$graph->Add($b1plot);
 
 
-		$b1plot->SetColor("white");
 
-		$graph->title->Set('Number of connection per day until the creations');
-		$graph->xaxis->title->Set('Day number');
-		$graph->yaxis->title->Set('Number of connections');
+	$b1plot->SetColor("white");
 
-		$graph->title->SetFont(FF_FONT1,FS_BOLD);
-		$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
-		$graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD);
+	$graph->title->Set('Number of connection per day until the creations');
+	$graph->xaxis->title->Set('Day number');
+	$graph->yaxis->title->Set('Number of connections');
 
-		$graph->Stroke();
+	$graph->title->SetFont(FF_FONT1,FS_BOLD);
+	$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
+	$graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD);
+
+
+	$gdImgHandler = $graph->Stroke(_IMG_HANDLER);
+	$fileName = "./users/$user/graphFreq.png";
+
+	$graph->img->Stream($fileName);
+	
 	}
 ?>
