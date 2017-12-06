8e957e7a60d3d846c40686112ec95d3a38b4673c
mattvil <mattvilain@gmail.com>
2017-12-07 00:26:03 +0100

    recherche user

--- bdd/panel/userview.php
+++ bdd/panel/userview.php
@@ -18,9 +18,44 @@
         <title>Facekey &mdash; Admin Panel</title>
     </head>
     <body>
-    <a href="add.php">Add New Data</a><br/><br/>
-    <a href="test.php">Test</a><br/><br/>
 
-    <?php echo display_table("users"); ?>
+      <a href="./index.php">Back<a>
+
+      <h3>RECHERCHE</h3>
+
+      <form method="post" action="userview.php">
+  			<input type="text" name="entre">
+  			<select name="option" id="option">
+  				<option value="id_user">ID_User</option>
+  				<option value="name">Name</option>
+          <option value="pseudo">Pseudo</option>
+          <option value="mail">Mail</option>
+  			</select>
+  			<input type="submit" name="valid" value="Recherche">
+  		</form>
+
+      <?php
+        if (isset($_POST['entre']) && isset($_POST['option'])){
+          $choise = $_POST['option'];
+          $search = $_POST['entre'];
+          $query = "SELECT * FROM Users WHERE $choise = '$search' ORDER BY id_user;";
+          //echo $query;
+          echo display_table_query($query);
+        }
+        else{
+          echo "<p>Veuillez renseigner tous les champs de recherche</p>\n";
+      }?>
+
+      <h3>AJOUTER DONNEES</h3>
+
+      <a href="add.php">Add New Data</a><br/><br/>
+
+
+
+      <!--<a href="test.php">Test</a><br/><br/> -->
+
+      <h3>LISTE TOUT LES USERS</h3>
+
+      <?php echo display_table("users"); ?>
     </body>
 </html>
