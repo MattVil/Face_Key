33707d4276ff7e713e2da75f9d2cd26897e65e2f
QuentGrd <quentin.gerard2@gmail.com>
2018-01-04 22:51:57 +0100

    test

--- reseau/dbutil.h
+++ reseau/dbutil.h
@@ -11,7 +11,7 @@
 #endif
 
 //#define CONN_INFO "host=10.40.128.23 port=5432 dbname=db2017l3i_mvilain user=y2017l3i_mvilain password=A123456# connect_timeout=10"
-#define CONN_INFO "host=127.0.0.1 port=5432 dbname=face_key user=postgres password= connect_timeout=10"
+#define CONN_INFO "host=127.0.0.1 port=5432 dbname=face_key user=parallels password=A123456# connect_timeout=10"
 
 void db_exit_nicely(PGconn *conn);
 void db_display_result(PGresult *result);
